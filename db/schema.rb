25172efc35001c61c6fd3ad480ea1de1ea35c63d
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-05 10:53:33 -0700

    omniauth migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_174812) do
+ActiveRecord::Schema.define(version: 2020_09_05_175225) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -27,8 +27,12 @@ ActiveRecord::Schema.define(version: 2020_09_05_174812) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider"], name: "index_users_on_provider"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid"], name: "index_users_on_uid"
   end
 
 end
