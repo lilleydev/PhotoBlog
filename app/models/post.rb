e546f03b55bfa2420c94b985b1e65d3318390172
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-05 15:11:30 -0700

    update user model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   resourcify
   belongs_to :author, class_name: 'User'
+  include Authority::Abilities
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  rolify
+  # rolify
+  resourcify
+  include Authority::Abilities
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
