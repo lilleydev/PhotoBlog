f47acc43275ee6b8239cf2ded68f3f89947661e4
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-05 11:15:48 -0700

    initialize omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   root 'posts#index'
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
