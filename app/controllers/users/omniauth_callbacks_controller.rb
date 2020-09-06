f780cf881853e5ef097ed06b1f0a1237cb74586e
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-06 10:40:46 -0700

    add devise views

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,12 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-    sign_in_and_redirect @user
+    user = User.from_omniauth(request.env['omniauth.auth'])
+    if user.persisted?
+      sign_in_and_redirect root_path, notice: 'Signed In'
+
+    else
+      session['devise.user_attributes'] = user.attributes
+      redirect_to new_user_registration_path
+    end
   end
 end
