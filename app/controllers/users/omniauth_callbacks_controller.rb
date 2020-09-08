aa83f274543281c40e16ab984f3fc3d97006daff
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-07 20:33:10 -0700

    working on fb auth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     user = User.from_omniauth(request.env['omniauth.auth'])
     if user.persisted?
-      sign_in_and_redirect root_path, notice: 'Signed In'
+      flash.notice = 'Sign in successful!'
+      sign_in_and_redirect root_path
 
     else
       session['devise.user_attributes'] = user.attributes
